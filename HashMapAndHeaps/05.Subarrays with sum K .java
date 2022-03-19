01f875a80dc6266fb1159c74fc978f21ec0e4b6f
Tamanna Verma <tamannav01@gmail.com>
2022-03-19 17:27:22 +0530

    Create 05.Subarrays with sum K .java

---
+++ HashMapAndHeaps/05.Subarrays with sum K .java
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int Arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                Arr[i] = Integer.parseInt(input_line[i]);
+            int k = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findSubArraySum(Arr, N, k));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int findSubArraySum(int nums[], int N, int k)
+    {
+          int sum=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        int ans=0;
+        for(int val:nums)
+        {
+            sum+=val;
+            if(map.containsKey(sum-k))
+            {
+                int freq=map.get(sum-k);
+                ans+=freq;
+               
+            }
+            
+            if(map.containsKey(sum))
+            {
+                map.put(sum,map.get(sum)+1);
+            }
+            else{
+                map.put(sum,1);
+            }
+        }
+        return ans;
+    }
+}
